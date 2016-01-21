c4641e1d643eab4ddc3a52ae5f7cbe85d26ce0ee
hqmq <michael@riesd.com>
2016-01-21 15:46:26 -0700

    setup dependencies

--- mix.exs
+++ mix.exs
@@ -10,23 +10,14 @@ defmodule ElixirRoombot.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:websocket_client, "~> 1.1.0"},
+      {:poison, "~> 1.5"},
+    ]
   end
 end
